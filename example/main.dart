9281e8dd7afa61a2e33c3b8826d4525851cbaa59
miyaji555 <kou.sepak@gmail.com>
2023-11-11 17:43:32 +0900

    Create web socket counter

--- example/main.dart
+++ example/main.dart
@@ -1,16 +1,16 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main() {
-  // Connect to the remote WebSocket endpoint.
-  final uri = Uri.parse('ws://localhost:8080/ws');
-  final channel = WebSocketChannel.connect(uri);
-
-  // Listen to messages from the server.
+void main() async {
+  final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8080/ws'));
   channel.stream.listen(print);
 
-  // Send a message to the server.
-  channel.sink.add('hello from the client');
+  channel.sink.add('__increment__');
+  channel.sink.add('__increment__');
+  channel.sink.add('__increment__');
+  channel.sink.add('__increment__');
+  channel.sink.add('__increment__');
+  channel.sink.add('__increment__');
+  channel.sink.add('__decrement__');
 
-  // Close the connection.
   channel.sink.close();
 }
