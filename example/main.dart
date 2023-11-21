8ca14c73e35dbcb95b7a80f5a67fcc1e1b1ecd83
miyaji555 <kou.sepak@gmail.com>
2023-11-22 08:55:04 +0900

    GoogleCloudRun上のAPIを叩けるようにした。

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,9 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() async {
-  final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8080/ws'));
+  // final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8080/ws'));
+  final channel = WebSocketChannel.connect(
+      Uri.parse('wss://gamesample-ibiwnvw3aa-an.a.run.app/ws'));
   channel.stream.listen(print);
 
   channel.sink.add('__increment__');
